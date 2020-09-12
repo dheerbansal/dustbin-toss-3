2b6512f4902c910da490653a8c05c74defb424fb
dheerbansal <67144062+dheerbansal@users.noreply.github.com>
2020-09-12 17:30:05 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,36 @@
+class Ball{
+    constructor(x,y,radius){
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+        this.radius = 50
+        this.x = x
+        this.y = y
+        this.image = loadImage("paper.png");
+        this.body = Bodies.circle(this.x,this.y,this.radius,options)
+        World.add(world,this.body)
+
+        
+
+        
+    }
+
+    
+    
+    display(){
+        var pos = this.body.position;
+       push();
+        translate(pos.x,pos.y);
+        rectMode(CENTER)
+        strokeWeight(3)
+        fill("purple")
+        image(this.image, 0, 0, this.radius,this.radius);
+     // image(0,0,this.radius,this.radius)
+        pop();
+        
+        
+              }
+            }
---
+++ sketch.js
@@ -0,0 +1,94 @@
+var  ball1, obstacle1,obstacle2,obstacle3,ground, dustbinIMG, slingshot, obstacle4,dustbin, slingshot
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+const Render = Matter.Render
+
+function preload(){
+	dustbinIMG = loadImage("dustbingreen.png");
+}
+
+
+function setup() {
+	createCanvas(1200, 800);
+
+    engine = Engine.create();
+	world = engine.world;
+
+	ball1 = new Ball(100,50,40,40);
+
+	obstacle1 = new Target(835,720,20,120);
+	obstacle1.visible = false
+	//obstacle1.shapeColor = "white";
+	obstacle2 =new Target(920,790,150,20);
+	obstacle2.visible = false
+	obstacle4 = new Target(400,500,30,20)
+	//obstacle2.shapeColor = "white";
+	obstacle3 = new Target(1000,720,20,150);
+	obstacle3.visible = false
+	//obstacle3.shapeColor = "white";
+
+	dustbin = createSprite(950,700,400,750);
+	dustbin.addImage(dustbinIMG);
+	dustbin.scale = 0.6
+	
+	ground = new Ground(600,790,1200,20);
+	ground.shapeColor = "purple";
+
+	slingshot = new SlingShot(100,500,ball1.body)
+
+	
+
+	var render = Render.create({
+		element:document.body,
+		engine: engine,
+		options:{
+			width:1200,
+			height:700,
+			wireframes:false
+		}
+	});
+	Engine.run(engine);
+  
+}
+
+function draw() {
+Engine.update(engine)
+  rectMode(CENTER);
+  background("white");
+  //ellipse();
+  
+
+  //keyPressed();
+
+  ground.display();
+  ball1.display();
+ // line(ball1.body.position.x,ball1.body.position.y,200,100)
+ // slingshot.display();
+ // line(ball1.body.position.x,ball1.body.position.y,obstacle4.body.position.x,obstacle4.body.position.y)
+
+  slingshot.display();
+  
+ // obstacle1.display();
+  //obstacle2.display();
+  //obstacle3.display();
+  
+ 
+  
+  drawSprites();
+ 
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(ball1.body,{x:mouseX,y:mouseY})
+}
+
+function mouseReleased(){
+   slingshot.fly();
+}
+
+
+
+
